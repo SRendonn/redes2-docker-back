c7d53e74ff800e612f7d05835daeda5d45028195
Sebastian Rendon <sebasrg0505@gmail.com>
2021-08-28 08:47:21 -0500

    feat: SecretLoader

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { QuoteModule } from './quote/quote.module';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env' }),
---
+++ src/config/SecretLoader.ts
@@ -0,0 +1,12 @@
+// dependencies
+import fs from 'fs';
+
+export const SecretLoader = {
+  read(secretName: string): string | undefined {
+    try {
+      return fs.readFileSync(`/run/secrets/${secretName}`, 'utf8');
+    } catch (err) {
+      return undefined;
+    }
+  },
+};
